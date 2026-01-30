9c9c8771db24ab848587e2628e099d0aed647307
Nithin Chowdary <nithinchowdary2354@gmail.com>
2026-01-30 22:33:06 +0530

    time zone set

--- server/utils/helpers.js
+++ server/utils/helpers.js
@@ -53,25 +53,34 @@ const normalizeUrl = (url) => {
  * Get time until next scheduled run
  */
 const getTimeUntilNext = (cronExpression) => {
-    // Simple calculation for common times (2AM, 4AM, 6AM)
+    // Current time in UTC
     const now = new Date();
-    const currentHour = now.getHours();
 
+    // IST Offset: UTC + 5:30 (19800000 ms)
+    const istOffset = 5.5 * 60 * 60 * 1000;
+
+    // Create a "Virtual IST Date" by shifting time
+    // We use UTC methods on this object to get IST values
+    const istDate = new Date(now.getTime() + istOffset);
+
+    const currentHour = istDate.getUTCHours();
+
+    // Scheduled times in IST (2 AM, 4 AM, 6 AM)
     const scheduledHours = [2, 4, 6];
 
     for (const hour of scheduledHours) {
         if (hour > currentHour) {
-            const next = new Date(now);
-            next.setHours(hour, 0, 0, 0);
-            return next.getTime() - now.getTime();
+            const next = new Date(istDate);
+            next.setUTCHours(hour, 0, 0, 0);
+            return next.getTime() - istDate.getTime();
         }
     }
 
-    // Next day 2AM
-    const tomorrow = new Date(now);
-    tomorrow.setDate(tomorrow.getDate() + 1);
-    tomorrow.setHours(2, 0, 0, 0);
-    return tomorrow.getTime() - now.getTime();
+    // If passed all times, schedule for next day 2 AM IST
+    const next = new Date(istDate);
+    next.setUTCDate(next.getUTCDate() + 1);
+    next.setUTCHours(2, 0, 0, 0);
+    return next.getTime() - istDate.getTime();
 };
 
 module.exports = {
